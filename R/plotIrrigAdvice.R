09efc346dc6be2d20dc54b5e4c360eaca142c227
GiulioGenova <giuliogenova89@gmail.com>
2019-08-13 12:11:23 +0200

    translated irrig advice legend into german

--- R/plotIrrigAdvice.R
+++ R/plotIrrigAdvice.R
@@ -15,22 +15,36 @@ plotIrrigAdvice <- function(db, wthrIcns=T){
 
 
   #db$img<-"http://daten.buergernetz.bz.it/services/weather/graphics/icons/imgsource/wetter/icon_18.png"
-  db <- db %>% mutate(TimeStamp = factor(TimeStamp),
-                      TimeStamp = factor(TimeStamp,
-                                         levels = rev(levels(TimeStamp))))
+
+  MustIrrig="Muss\nbewässern"
+  NoIrrig="Keine\nbewässerung"
+  SugIrrig="Bewässerung\nvorschlagen"
+
+  fill_values=c("Muss\nbewässern"="red", "Keine\nbewässerung"="green","Bewässerung\nvorschlagen"="orange")
+
+  db <- db %>% mutate(
+    TimeStamp=substr(TimeStamp,6,10),
+    TimeStamp = factor(TimeStamp),
+    TimeStamp = factor(TimeStamp,
+                       levels = rev(levels(TimeStamp))),
+    irrigAdvise = case_when(
+      irrigAdvise=="MustIrrig" ~ MustIrrig,
+      irrigAdvise=="NoIrrig" ~ NoIrrig,
+      irrigAdvise=="SugIrrig" ~ SugIrrig
+    ))
 
   p <- ggplot(db, aes(y=TimeStamp)) +#
     labs(x=NULL,y=NULL,fill=NULL)+
-    geom_tile(aes(x="1",fill=irrigAdvise,width=1),#hjust = 0.1,, height=1
+    geom_tile(aes(x="1",fill=irrigAdvise,width=0.8),#hjust = 0.1,, height=1
               color="white",show.legend = T,size=1.5)#
   #coord_fixed(ratio = 1)
-  if (wthrIcns){p<- p + geom_image(x = 1.8, aes(image = img),size=.15) }
+  if (wthrIcns){p<- p + geom_image(x = 1.6, aes(image = img),size=.15) }
 
-    #coord_flip() +
-    #expand_limits(x = c(1,1.1))  +
-    p  + coord_cartesian(xlim = c(0.98, 2),expand = F)+
+  #coord_flip() +
+  #expand_limits(x = c(1,1.1))  +
+  p  + coord_cartesian(xlim = c(0.98, 2),expand = F)+
     #scale_y_reverse()+
-    scale_fill_manual(values=c("MustIrrig"="red", "NoIrrig"="green","SugIrrig"="orange"))+
+    scale_fill_manual(values=fill_values)+
     #coord_flip()+
     theme(panel.background = element_rect(fill = NA),
           panel.grid.major = element_blank(),#element_line(linetype = "dashed",colour = "grey"),
@@ -42,7 +56,7 @@ plotIrrigAdvice <- function(db, wthrIcns=T){
           legend.position = "top",#,
 
           # legend.title = element_text(size=legend_text_size),
-          legend.text = element_text(size = 20)
+          legend.text = element_text(size = 13)
           # legend.key = element_rect(colour = "#f1fafc"),#size = 2
           # legend.key.size = unit(3, 'lines'),
           # axis.line.x = element_line(colour = "black",size=1.2)
